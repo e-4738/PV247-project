29f4d68d4c3bdf9f8ad812fac20789043c1d5549
Erik Hricisak <xhricis@fi.muni.cz>
2023-04-28 16:40:35 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { ThemeProvider } from '@emotion/react';
+import { Container, CssBaseline } from '@mui/material';
+import {
+	Outlet,
+	RootRoute,
+	Route,
+	Router,
+	RouterProvider
+} from '@tanstack/react-router';
+
+import { theme } from './theme';
+import Home from './components/Home';
+
+const rootRoute = new RootRoute({
+	component: () => (
+		<>
+			<CssBaseline />
+			<Container
+				component="main"
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexGrow: 1,
+					gap: 2
+				}}
+			>
+				<Outlet />
+			</Container>
+		</>
+	)
+});
+
+const indexRoute = new Route({
+	getParentRoute: () => rootRoute,
+	path: '/',
+	component: Home
+});
+
+const routeTree = rootRoute.addChildren([indexRoute]);
+
+const router = new Router({ routeTree });
+
+declare module '@tanstack/react-router' {
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const App = () => (
+	<ThemeProvider theme={theme}>
+		<RouterProvider router={router} />
+	</ThemeProvider>
+);
+
+export default App;
---
+++ src/components/Home.tsx
@@ -0,0 +1,5 @@
+import { Typography } from '@mui/material';
+
+const Home = () => <Typography variant="h1">Let the games begin</Typography>;
+
+export default Home;
