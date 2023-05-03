3163bd37a011b74767656041aa13308af5221265
Zuzana Kollarova <zuzana.kollarova@ivyassistant.com>
2023-05-03 08:46:41 +0200

    add firebase stubs

---
+++ src/hooks/useLoggedInUser.tsx
@@ -0,0 +1,30 @@
+// import {
+// 	createContext,
+// 	FC,
+// 	PropsWithChildren,
+// 	useContext,
+// 	useEffect,
+// 	useState
+// } from 'react';
+// import { User } from 'firebase/auth';
+
+// import { onAuthChanged } from '../firebase';
+
+// const UserContext = createContext<User | undefined>(undefined);
+
+// export const UserProvider: FC<PropsWithChildren> = ({ children }) => {
+// 	// Hold user info in state
+// 	const [user, setUser] = useState<User>();
+
+// 	// Setup onAuthChanged once when component is mounted
+// 	useEffect(() => {
+// 		onAuthChanged(u => setUser(u ?? undefined));
+// 	}, []);
+
+// 	return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
+// };
+
+// // Hook providing logged in user information
+// const useLoggedInUser = () => useContext(UserContext);
+
+// export default useLoggedInUser;
