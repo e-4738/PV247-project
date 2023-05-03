bcdd1aa90f7760305f146b21d5fff6c6feccaa8b
Zuzana Kollarova <zuzana.kollarova@ivyassistant.com>
2023-05-03 10:33:17 +0200

    wip GuessSong.tsx component

--- src/pages/PlayQuiz.tsx
+++ src/pages/PlayQuiz.tsx
@@ -1,13 +1,17 @@
 import { Typography } from '@mui/material';
 
 import usePageTitle from '../hooks/usePageTitle';
+import GuessSong from '../components/GuessSong';
 
 const PlayQuiz = () => {
 	usePageTitle('Play');
 	return (
-		<Typography variant="h1">
-			This is where the PlayQuiz page will be.
-		</Typography>
+		<>
+			<Typography variant="h1">
+				This is where the PlayQuiz page will be.
+			</Typography>
+			<GuessSong />
+		</>
 	);
 };
 
