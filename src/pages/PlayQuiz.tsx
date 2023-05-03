e4fd75f78743f9e60dcf8b53cacf1dc2ddf55993
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-05-03 22:58:59 +0200

    added lyrics generation hook

--- src/pages/PlayQuiz.tsx
+++ src/pages/PlayQuiz.tsx
@@ -2,11 +2,15 @@ import { Typography } from '@mui/material';
 
 import usePageTitle from '../hooks/usePageTitle';
 import GuessSong from '../components/GuessSong';
+import useLyricsGenerator from '../hooks/useLyricsGenerator';
 
 const PlayQuiz = () => {
 	usePageTitle('Play');
+	const quote = useLyricsGenerator();
+
 	return (
 		<>
+			<Typography>{quote}</Typography>
 			<Typography variant="h1">
 				This is where the PlayQuiz page will be.
 			</Typography>
