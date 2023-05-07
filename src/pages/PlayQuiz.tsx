2a497b7d63b2c2fbc4eeb10bf6f9a566f017586c
Erik Hricisak <xhricis@fi.muni.cz>
2023-05-07 18:35:03 +0200

    Create playlist screen with spotify data

--- src/pages/PlayQuiz.tsx
+++ src/pages/PlayQuiz.tsx
@@ -5,52 +5,11 @@ import usePageTitle from '../hooks/usePageTitle';
 import { useSpotifyAuth } from '../hooks/useSpotifyAuth';
 import Playlist from '../components/Playlist';
 
-type SpotifyPlaylists = {
-	href: string;
-	limit: number;
-	next: string;
-	offset: number;
-	previous: string;
-	total: number;
-	items: Array<{
-		collaborative: boolean;
-		description: string;
-		external_urls: { spotify: string };
-		href: string;
-		id: string;
-		images: Array<{
-			url: string;
-			height: number;
-			width: number;
-		}>;
-		name: string;
-		owner: {
-			external_urls: { spotify: string };
-			followers: { href: string; total: number };
-			href: string;
-			id: string;
-			type: string;
-			uri: string;
-			display_name: string;
-		};
-		public: boolean;
-		snapshot_id: string;
-		tracks: {
-			href: string;
-			total: number;
-		};
-		type: string;
-		uri: string;
-	}>;
-};
-
 const PlayQuiz = () => {
 	usePageTitle('Play');
 	const [accessToken] = useSpotifyAuth();
 
-	console.log(accessToken);
-
-	const query = useQuery({
+	const { data } = useQuery({
 		queryKey: ['playlists'],
 		queryFn: () =>
 			fetch('https://api.spotify.com/v1/browse/categories/pop/playlists', {
@@ -61,16 +20,12 @@ const PlayQuiz = () => {
 			}).then(res => res.json())
 	});
 
-	console.log(query);
-	console.log(query.error);
-	const data: SpotifyPlaylists = query.data;
-
 	return (
 		<>
 			<Typography variant="h1">Pick your playlist</Typography>
-			<Box sx={{ display: 'flex' }}>
-				{data.items.map((item, key) => (
-					<Playlist key={key} name={item.name} />
+			<Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+				{data?.playlists?.items?.map((item, key) => (
+					<Playlist key={key} data={item} />
 				))}
 			</Box>
 		</>
