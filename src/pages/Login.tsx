c8ddfee1e8cc890fac8029830b26f0216b14cb4c
Zuzana Kollarova <zuzana.kollarova@ivyassistant.com>
2023-05-07 21:20:25 +0200

    redo login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -79,50 +79,23 @@ const Login = () => {
 			<Typography variant="h4" textAlign="center" mb={3}>
 				User Login
 			</Typography>
-			<TextField label="email" {...email.props} type="email" />
-			<TextField label="password" {...password.props} type="password" />
-			<Box
-				sx={{
-					display: 'flex',
-					gap: 2,
-					alignItems: 'center',
-					alignSelf: 'flex-end',
-					mt: 2
-				}}
+
+			<Button
+				type="submit"
+				variant="outlined"
+				onClick={() => console.log('Redirect to spotify auth')}
 			>
-				{submitError && (
-					<Typography
-						variant="caption"
-						textAlign="right"
-						sx={{ color: 'error.main' }}
-					>
-						{submitError}
-					</Typography>
-				)}
-				<Button
-					type="submit"
-					variant="outlined"
-					onClick={() => setSignUp(true)}
-				>
-					Sign Up & Connect To
-					<Box ml="8px">
-						<Image
-							src="spotifyLogo.png"
-							alt="Log in with Spotify."
-							width="90px"
-							duration={0}
-							bgColor="none"
-						/>
-					</Box>
-				</Button>
-				<Button
-					type="submit"
-					variant="contained"
-					onClick={() => setSignUp(false)}
-				>
-					Sign In
-				</Button>
-			</Box>
+				Sign In With
+				<Box ml="8px">
+					<Image
+						src="spotifyLogo.png"
+						alt="Log in with Spotify."
+						width="90px"
+						duration={0}
+						bgColor="none"
+					/>
+				</Box>
+			</Button>
 		</Paper>
 	);
 };
