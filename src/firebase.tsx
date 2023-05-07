25a33f0468130c7786c1b58e59c3bb0667eca061
JuliaJamnicka <julia.jamnicka@gmail.com>
2023-05-07 21:35:34 +0200

    delete users table, add games table to firebase db

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,13 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import {
-	getAuth,
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	signOut as authSignOut,
-	onAuthStateChanged,
-	User
-} from 'firebase/auth';
-import {
 	collection,
 	CollectionReference,
 	doc,
@@ -26,79 +18,24 @@ initializeApp({
 	appId: '1:116546591898:web:03da99b045ea5cbcc81323'
 });
 
-// Authentication
-const auth = getAuth();
-
-// Sign up handler
-export const signUp = (email: string, password: string) =>
-	createUserWithEmailAndPassword(auth, email, password);
-
-// Sign in handler
-export const signIn = (email: string, password: string) =>
-	signInWithEmailAndPassword(auth, email, password);
-
-// Sign out handler
-export const signOut = () => authSignOut(auth);
-
-// Subscribe to auth state changes
-export const onAuthChanged = (callback: (u: User | null) => void) =>
-	onAuthStateChanged(auth, callback);
-
 // Firestore
 const db = getFirestore();
 
-export type SpotifyUser = {
-	mail: string;
+export type Game = {
 	spotifyUserId: string;
-	displayName: string;
-	image: string;
-	profileLink: string;
-	accessToken: string;
-	refreshToken: string;
+	spotifyDisplayName: string;
+	spotifyUserProfileLink: string;
+	userProfilePictureLink: string;
+	playlistName: string;
+	playlistLink: string;
+	score: number;
+	duration: number;
 };
 
-export const usersCollection = collection(
+export const gamesCollection = collection(
 	db,
-	'users'
-) as CollectionReference<SpotifyUser>;
+	'games'
+) as CollectionReference<Game>;
 
-export const userDocument = (mail: string) =>
-	doc(db, 'users', mail) as DocumentReference<SpotifyUser>;
-
-export const getSpotifyUserFromDB = async (mail?: string) => {
-	if (!mail) return null;
-
-	const docSnap = await getDoc(userDocument(mail));
-
-	if (docSnap.exists()) {
-		const SU: SpotifyUser = {
-			mail: docSnap.get('mail'),
-			spotifyUserId: docSnap.get('spotifyUserId'),
-			displayName: docSnap.get('displayName'),
-			image: docSnap.get('image'),
-			profileLink: docSnap.get('profileLink'),
-			accessToken: docSnap.get('accessToken'),
-			refreshToken: docSnap.get('refreshToken')
-		};
-		return SU;
-	} else {
-		console.log(`could not retriev user with email${mail}`);
-		return null;
-	}
-};
-
-export const getRefreshToken = async (
-	mail: string
-): Promise<string | undefined> => {
-	const docSnap = await getDoc(userDocument(mail));
-
-	return docSnap.get('refreshToken');
-};
-
-export const getAccessToken = async (
-	mail: string
-): Promise<string | undefined> => {
-	const docSnap = await getDoc(userDocument(mail));
-
-	return docSnap.get('accessToken');
-};
+export const gameDocument = (id: string) =>
+	doc(db, 'games', id) as DocumentReference<Game>;
