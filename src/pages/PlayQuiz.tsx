db9d10ab63267be888bb154dce20896b5fe7df46
Erik Hricisak <xhricis@fi.muni.cz>
2023-05-08 12:28:02 +0200

    Add different categories of playlists

--- src/pages/PlayQuiz.tsx
+++ src/pages/PlayQuiz.tsx
@@ -1,5 +1,11 @@
-import { Avatar, Box, Typography } from '@mui/material';
+import {
+	Box,
+	ToggleButton,
+	ToggleButtonGroup,
+	Typography
+} from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
+import { SetStateAction, useState } from 'react';
 
 import usePageTitle from '../hooks/usePageTitle';
 import Playlist from '../components/Playlist';
@@ -13,24 +19,71 @@ export type SpotifyPlaylist = {
 	}>;
 };
 
+type Category = 'pop' | 'alternative' | 'mood' | 'decades' | 'party';
+
+type CategoryDescription = Record<Category, string>;
+
 const PlayQuiz = () => {
 	usePageTitle('Play');
 	const user = useLoggedInUser();
 
+	const categoryDescription: CategoryDescription = {
+		pop: 'Guess the song from the latest and greatest pop hits from around the world.',
+		alternative:
+			'Listen to alternative music and test your knowledge by guessing the song.',
+		mood: 'Identify songs based on their mood and vibe.',
+		decades:
+			'Travel through time and guess the iconic hits from different decades.',
+		party:
+			'Get the party started and see if you can guess the songs from these upbeat party anthems.'
+	};
+
+	const [category, setCategory] = useState<Category>('pop');
+
 	const { data } = useQuery({
-		queryKey: ['playlists'],
+		queryKey: [category],
 		queryFn: () =>
-			fetch('https://api.spotify.com/v1/browse/categories/pop/playlists', {
-				method: 'GET',
-				headers: {
-					Authorization: `Bearer ${user?.accessToken}`
+			fetch(
+				`https://api.spotify.com/v1/browse/categories/${category}/playlists`,
+				{
+					method: 'GET',
+					headers: {
+						Authorization: `Bearer ${user?.accessToken}`
+					}
 				}
-			}).then(res => res.json())
+			).then(res => res.json())
 	});
+
+	const changeCategory = (
+		_event: React.MouseEvent<HTMLElement>,
+		newCategory: SetStateAction<Category>
+	) => {
+		if (newCategory !== null) {
+			setCategory(newCategory);
+		}
+	};
 
 	return (
 		<>
 			<Typography variant="h1">Pick your playlist</Typography>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					gap: 1
+				}}
+			>
+				<ToggleButtonGroup value={category} exclusive onChange={changeCategory}>
+					{Object.keys(categoryDescription).map(category => (
+						<ToggleButton key={category} value={category}>
+							<Typography variant="subtitle1">{category}</Typography>
+						</ToggleButton>
+					))}
+				</ToggleButtonGroup>
+				<Typography variant="h5">{categoryDescription[category]}</Typography>
+			</Box>
+
 			<Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
 				{data?.playlists?.items?.map((item: SpotifyPlaylist, key: number) => (
 					<Playlist key={key} playlist={item} />
@@ -38,7 +91,6 @@ const PlayQuiz = () => {
 			</Box>
 		</>
 	);
-
 };
 
 export default PlayQuiz;
