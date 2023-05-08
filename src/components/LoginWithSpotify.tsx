5042dbd5270e796404fc2a18ecdd6ce38e6b0c87
Zuzana Kollarova <zuzana.kollarova@ivyassistant.com>
2023-05-08 13:21:52 +0200

    add login to main page

---
+++ src/components/LoginWithSpotify.tsx
@@ -0,0 +1,26 @@
+import { Button, Box } from '@mui/material';
+import { Image } from 'mui-image';
+import { FC } from 'react';
+
+import { getSpotifyAuthorizationCode } from '../utils/spotifyAuthorizationUtils';
+
+const LoginWithSpotify: FC = () => (
+	<Button
+		type="submit"
+		variant="outlined"
+		onClick={getSpotifyAuthorizationCode}
+	>
+		Sign In With
+		<Box ml="8px">
+			<Image
+				src="spotifyLogo.png"
+				alt="Log in with Spotify."
+				width="90px"
+				duration={0}
+				bgColor="none"
+			/>
+		</Box>
+	</Button>
+);
+
+export default LoginWithSpotify;
