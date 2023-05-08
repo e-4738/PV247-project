4f5e0f8929c90de3150f16f5170474c06c921922
Zuzana Kollarova <zuzana.kollarova@ivyassistant.com>
2023-05-08 20:47:27 +0200

    add leaderboard stubs to leaderboard page

--- src/pages/LeaderBoard.tsx
+++ src/pages/LeaderBoard.tsx
@@ -1,11 +1,44 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import usePageTitle from '../hooks/usePageTitle';
+import LeaderBoardItem from '../components/LeaderBoardItem';
+import useLoggedInUser from '../hooks/useLoggedInUser';
 
 const LeaderBoard = () => {
 	usePageTitle('Leader Board');
+	const user = useLoggedInUser();
 	return (
-		<Typography variant="h1">This is where the leaderboard will be.</Typography>
+		<>
+			<Typography variant="h4" textAlign="center">
+				Leader Board
+			</Typography>
+
+			{user && (
+				<Box
+					sx={{
+						width: '100%',
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						gap: 2
+					}}
+				>
+					{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((position, key) => (
+						<LeaderBoardItem
+							key={key}
+							position={position}
+							playerName={user?.displayName}
+							playerAvatarLink={user.image}
+							playerSpotifyProfileLink={user.profileLink}
+							playlistName="my life is a movie"
+							playlistLink="https://open.spotify.com/playlist/37i9dQZF1DX4OzrY981I1W"
+							gameScore={7}
+							gameDuration={12000}
+						/>
+					))}
+				</Box>
+			)}
+		</>
 	);
 };
 
